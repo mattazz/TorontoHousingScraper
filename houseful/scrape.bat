8738c70e9c90b4322f1fe25ad1ef5edd3015dab1
Matthew Azada <matthewazada101@gmail.com>
2024-03-02 13:41:30 -0500

    fully scraped zolo

--- houseful/scrape.bat
+++ houseful/scrape.bat
@@ -1,7 +1,7 @@
 @echo off
 setlocal enabledelayedexpansion
 
-set /a page_num=31
+set /a page_num=101
 
 :loop
 echo Running scrapy with page_num=!page_num!
@@ -9,4 +9,4 @@ scrapy crawl zolo -o listing.jl -a page_num=!page_num!
 timeout /t 5
 set /a page_num=!page_num!+1
 
-if !page_num! leq 100 goto loop
+if !page_num! leq 163 goto loop
