c751df354dcdb03b97c0f26bde0f8cf728cb69e8
Mattazz <matthewazada101@gmail.com>
2024-03-06 14:25:55 -0500

    fixing stuff

---
+++ houseful/houseful/spiders/zolo_links.py
@@ -0,0 +1,86 @@
+from typing import Iterable
+import random
+import scrapy
+
+
+class ZoloSpider(scrapy.Spider):
+    name = "zolo_links"
+    allowed_domains = ["zolo.ca"]
+
+    def __init__(self, page_num=None, *args, **kwargs):
+        super(ZoloSpider, self).__init__(*args, **kwargs)
+        self.page_num = page_num  # Access the argument
+
+    def start_requests(self):
+        urls = [f"https://www.zolo.ca/toronto-real-estate/page-{self.page_num}"]
+        cookies = {
+            "BID": "04431cd8-d662-11ee-92d1-bc764e102e1e",
+            "BSID": "f949c591-d778-11ee-92d1-bc764e102e1e",
+            "HREFR": "https%3A%2F%2Fwww.google.com%2F",
+            "SOT": "2",
+            "__cf_bm": "TbkMH10VvH0PrbWDNK0PanSamwUlN06pG04uNBhTrhE-1709263428-1.0-ASCVNcxJ3u+5Wb1tcYXnDVJUZ8lchAdJLUBtG+mo37aiGbYlSpcSQlO4F7ldvdY+yCEV5c+Q5fLloSSX/75maEs=",
+            "__cfruid": "6034dc890e521219c885c8945716979cf8848fa5-1709262507",
+            "_ga": "GA1.1.1166958093.1709142697",
+            "_ga_SRQSX6WNCC": "GS1.1.1709262509.2.1.1709262583.0.0.0",
+            "full-css": "true",
+            "id-": "",
+        }
+        headers = {
+            "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+            # "Accept-Encoding": "gzip, deflate, br",
+            "Accept-Language": "en-US,en;q=0.9",
+            "Cache-Control": "max-age=0",
+            "Referer": "https://www.zolo.ca/",
+            "Sec-Ch-Ua": '"Chromium";v="122", "Not(A:Brand";v="24", "Microsoft Edge";v="122"',
+            "Sec-Ch-Ua-Mobile": "?0",
+            "Sec-Ch-Ua-Platform": '"Windows"',
+            "Sec-Fetch-Dest": "document",
+            "Sec-Fetch-Mode": "navigate",
+            "Sec-Fetch-Site": "same-origin",
+            "Sec-Fetch-User": "?1",
+            "Upgrade-Insecure-Requests": "1",
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36 Edg/122.0.0.0",
+        }
+
+        # proxies = [
+        #     "http://45.181.123.201:999",
+        #     "http://181.81.245.194:4128",
+        #     "http://103.88.90.50:8080",
+        #     "http://103.152.232.74:8181",
+        #     "http://45.182.176.38:9947",
+        #     "http://190.120.249.180:999",
+        #     "http://103.141.180.254:80",
+        #     "http://94.131.14.66:3128",
+        #     "http://62.201.251.217:8585",
+        # ]
+
+        for url in urls:
+            # proxy = random.choice(proxies)
+            yield scrapy.Request(
+                url=url,
+                callback=self.parse,
+                cookies=cookies,
+                headers=headers,
+                # meta={"proxy": proxy},
+            )
+
+    def parse(self, response):
+        listings = []
+        for listing in response.css("a.address::attr(href)"):
+            listing_url = listing.get()
+            print("RESULT: " + listing_url)
+            listings.append(listing_url)
+            yield {"listing": listing_url}
+
+        # Find the next page URL
+
+        current_page_number = int(response.url.split("/")[-1].replace("page-", ""))
+        next_page_number = current_page_number + 1
+        next_page_url = (
+            f"https://www.zolo.ca/toronto-real-estate/page-{next_page_number}"
+        )
+
+        # Check if the next page exists by ensuring it doesn't exceed a max page limit
+        # or by checking the presence of a 'Next' button or similar indicator (not shown here)
+        if next_page_number <= 10:  # Example max limit; adjust or remove as necessary
+            yield scrapy.Request(url=next_page_url, callback=self.parse)
